ba5ea160702c38fcbd71984063f758bf269e2dd6
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-08-26 15:21:15 +0200

    Added fruitList component

---
+++ react-fundamentals/src/Component/fruitList.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class FruitList extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ol>
+          {this.props.fruits.map(element => (
+            <li>{element}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default FruitList;
