aecbf239faaad4e5bb30f26aeb7f205fd6844008
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-08-26 17:18:23 +0200

    Added react state property to Time component

---
+++ react-fundamentals/src/Component/timer.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Timer extends Component {
+  state = {
+    date: new Date()
+  };
+
+  callMe() {
+    setInterval(() => {
+      this.setState({ date: new Date() });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Current Time</h1>
+        <h2>{this.state.date.toLocaleTimeString()}</h2>
+        {this.callMe()}
+      </div>
+    );
+  }
+}
+
+export default Timer;
--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import FruitList from "./Component/fruitList";
+import Timer from "./Component/timer";
 
-ReactDOM.render(
-  <FruitList fruits={["Mango", "Banana", "Orange"]} />,
-  document.getElementById("root")
-);
+ReactDOM.render(<Timer />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
