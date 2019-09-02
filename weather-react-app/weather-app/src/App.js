3d7529e9dd581cc8d00d84a4b4ea508d97af4360
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-09-02 10:12:40 +0200

    Added weathericons.io library

--- weather-react-app/weather-app/src/App.js
+++ weather-react-app/weather-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <h5 className="py-4"></h5>
     </div>
   );
 }
