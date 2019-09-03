babbcb0c404f69a4b5f8d5e709854b21299f5ec2
Miroslav <miroslav.miki.stojakovic@gmail.com>
2019-09-03 12:47:35 +0200

    Added weather service api call

--- weather-react-app/weather-app/src/App.js
+++ weather-react-app/weather-app/src/App.js
@@ -1,15 +1,40 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "weather-icons/css/weather-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { async } from "q";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <h5 className="py-4"></h5>
-    </div>
-  );
+const ApiKey = "";
+// api.openweathermap.org/data/2.5/weather?q=London,uk
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.getWeather();
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=Novi%20Sad,RS&appId=${ApiKey}`
+    );
+
+    const response = await api_call.json();
+    console.log(response);
+  };
+
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <h1>Weather App</h1>
+        <h5 className="py-4">
+          <i className="wi wi-day-sunny"></i>
+        </h5>
+      </div>
+    );
+  }
 }
 
 export default App;
